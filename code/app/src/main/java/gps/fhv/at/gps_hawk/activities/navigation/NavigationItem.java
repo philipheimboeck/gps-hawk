2336ce4d9d86020bc62a719d4131d0cb5084f476
Philip Heimböck <philipheimboeck@gmail.com>
2015-10-23 12:37:43 +0200

    Drawer is now working

--- code/app/src/main/java/gps/fhv/at/gps_hawk/activities/navigation/NavigationItem.java
+++ code/app/src/main/java/gps/fhv/at/gps_hawk/activities/navigation/NavigationItem.java
@@ -1,5 +1,7 @@
 package gps.fhv.at.gps_hawk.activities.navigation;
 
+import android.app.Fragment;
+
 /**
  * Author: Philip Heimböck
  * Date: 22.10.15
@@ -9,18 +11,28 @@ public class NavigationItem {
     private String mText;
     private int mIconRes;
     private boolean mVisible;
+    private Fragment mFragment;
 
     public NavigationItem() {
     }
 
-    public NavigationItem(String text, int iconRes) {
-        this(text, iconRes, true);
+    public NavigationItem(Fragment fragment, String text, int iconRes) {
+        this(fragment, text, iconRes, true);
     }
 
-    public NavigationItem(String text, int iconRes, boolean visible) {
+    public NavigationItem(Fragment fragment, String text, int iconRes, boolean visible) {
+        mFragment = fragment;
         mText = text;
         mIconRes = iconRes;
         mVisible = visible;
+    }
+
+    public Fragment getFragment() {
+        return mFragment;
+    }
+
+    public void setFragment(Fragment fragment) {
+        mFragment = fragment;
     }
 
     public String getText() {
