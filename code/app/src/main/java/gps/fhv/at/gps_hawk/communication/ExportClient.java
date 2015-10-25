e3a3c3a44ec29ed4fbdb987f300591ee7839c1ca
tme5092 <tobias.metzler@students.fhv.at>
2015-10-25 14:46:46 +0100

    Export-Data (Task)

---
+++ code/app/src/main/java/gps/fhv/at/gps_hawk/communication/ExportClient.java
@@ -0,0 +1,60 @@
+package gps.fhv.at.gps_hawk.communication;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
+import gps.fhv.at.gps_hawk.domain.ExportContext;
+import gps.fhv.at.gps_hawk.exceptions.NoConnectionException;
+import gps.fhv.at.gps_hawk.exceptions.RegistrationException;
+
+/**
+ * Created by Tobias on 25.10.2015.
+ */
+public class ExportClient extends RestClient {
+
+
+    public ExportClient(Context context) {
+        super(context);
+    }
+
+    public boolean exportCollectedWaypoints(ExportContext expCtx) {
+
+        JSONObject object = new JSONObject();
+        try {
+            object.put("waypoints",expCtx.getWaypointList());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            URL url = new URL(expCtx.getUrl());
+
+            HashMap<String, String> params = new HashMap<>();
+            params.put("data", object.toString());
+
+            HTTPAnswer answer = post(url, params);
+            if (answer.responseCode != 200) {
+                throw new RegistrationException("Registration was not successful!");
+            }
+            return true;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RegistrationException e) {
+            e.printStackTrace();
+        } catch (NoConnectionException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
